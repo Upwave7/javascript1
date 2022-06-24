443172397cd26e3d1af2c4004171ba684ce448a6
Upwave7 <olajidetomori84@gmail.com>
2022-06-24 21:32:45 +0100

    second commit

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let thename = 'Tomori Olajide'
 let height = '5ft, 8inches'
 let country = 'Nigeria'
 
+document.write( 'My name is ', thename )
+document.write("<br>");
+document.write( 'My height is ',  height )
+document.write("<br>");
+document.write( 'Mycountry is ',  country )
 
  console.log(thename);
  console.log(height);
