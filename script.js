386f5c03fde583ce02d5c718675158ac60c510e9
Upwave7 <olajidetomori84@gmail.com>
2022-06-24 17:51:06 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let thename = 'Tomori Olajide'
+let height = '5ft, 8inches'
+let country = 'Nigeria'
+
+
+ console.log(thename);
+ console.log(height);
+ console.log(country);
+
+//  for(let i = 0; i <= 1000; i += 1){
+//     console.log(i);
+//  }
